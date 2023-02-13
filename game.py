438d71faa7c19934ccc5e1fecfb10d2c28c43369
june <djie627@gmail.com>
2023-02-13 13:57:33 +1100

    Stage 3/6: Where to next?

--- game.py
+++ game.py
@@ -72,17 +72,42 @@ def valid_position(matrix: list) -> (int, int):
     return num1, num2
 
 
+def move_directions(x: int, y: int, matrix: list):
+    moves = [(2, 1), (2, -1), (-2, 1), (-2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2)]
+    landing_position = []
+    for i, j in moves:
+        move_x = x + i
+        move_y = y + j
+        if 0 < move_x <= len(matrix[0]) and 0 < move_y <= len(matrix):
+            landing_position.append([move_x, move_y])
+
+    return landing_position
+
+
 def main():
     dimension1, dimension2 = valid_dimensions()
     board = chess_board(dimension1, dimension2)
     start_position1, start_position2 = valid_position(board)
+
     if dimension1 * dimension2 >= 100:
         board[- start_position2][start_position1 - 1] = "  X"
     elif 100 > dimension1 * dimension2 >= 10:
         board[- start_position2][start_position1 - 1] = " X"
     else:
         board[- start_position2][start_position1 - 1] = "X"
+
+    moves = move_directions(start_position1, start_position2, board)
+    # print(moves)
+    for h, v in moves:
+        if dimension1 * dimension2 >= 100:
+            board[-v][h - 1] = "  0"
+        elif 100 > dimension1 * dimension2 >= 10:
+            board[-v][h - 1] = " 0"
+        else:
+            board[-v][h - 1] = "0"
+
     knight_move = boarder_chess_board(board)
+    print("\nHere are the possible moves:")
     for row in knight_move:
         if isinstance(row, str):
             print(row)
