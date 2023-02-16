e89b794fd61021ab372373f38e69b64e2dfaad52
june <djie627@gmail.com>
2023-02-16 16:33:39 +1100

    Stage 5/6: reorganise the code, allow multiple moves and mark the squares you have already visited

--- game.py
+++ game.py
@@ -1,49 +1,9 @@
-def chess_board(x: int, y: int) -> list:
-    matrix = []
-    for i in range(y):
-        row = []
-        for j in range(x):
-            if x * y >= 100:
-                row.append("___")
-            elif 100 > x * y >= 10:
-                row.append("__")
-            else:
-                row.append("_")
-        matrix.append(row)
-    return matrix
-
-
-def boarder_chess_board(matrix: list) -> list:
-    num_cells = len(matrix) * len(matrix[0])
-
-    for i in range(len(matrix)):
-        matrix[i].insert(0, str(len(matrix) - i) + "|")
-        matrix[i].append("|")
-    if num_cells >= 100:
-        matrix.append(["  " + str(i) for i in range(1, len(matrix[0]) - 1)])
-        matrix[len(matrix) - 1].insert(0, "  ")
-        boarder_length = (len(matrix[0]) - 2) * 4 + 3
-    elif 100 > num_cells >= 10:
-        matrix.append([" " + str(i) for i in range(1, len(matrix[0]) - 1)])
-        matrix[len(matrix) - 1].insert(0, "  ")
-        boarder_length = (len(matrix[0]) - 2) * 3 + 3
-    else:
-        matrix.append([str(i) for i in range(1, len(matrix[0]) - 1)])
-        matrix[len(matrix) - 1].insert(0, "  ")
-        boarder_length = (len(matrix[0]) - 2) * 2 + 3
-
-    matrix.insert(0, " " + boarder_length * "-")
-    matrix.insert(-1, " " + boarder_length * "-")
-
-    return matrix
-
-
 def valid_dimensions() -> (int, int):
     invalid_input = False
     while not invalid_input:
         try:
-            x, y = map(int, input("Enter your board dimensions: ").split())
-            if x < 0 or y < 0:
+            dim_x, dim_y = map(int, input("Enter your board dimensions: ").split())
+            if dim_x <= 0 or dim_y <= 0:
                 print("Invalid dimensions!")
                 continue
             else:
@@ -51,17 +11,20 @@ def valid_dimensions() -> (int, int):
         except ValueError:
             print("Invalid dimensions!")
 
-    return x, y
+    return dim_x, dim_y
 
 
-def valid_position(matrix: list) -> (int, int):
-    x = len(matrix[0])
-    y = len(matrix)
+def input_position_to_board_position(input_position, dim_y):
+    return input_position[0] - 1, dim_y - input_position[1]
+
+
+def valid_position(dim_x: int, dim_y: int) -> (int, int):
     invalid_input = False
     while not invalid_input:
         try:
-            num1, num2 = map(int, input("Enter the knight's starting position: ").split())
-            if num1 not in range(1, x + 1) or num2 not in range(1, y + 1):
+            input_x, input_y = map(int, input("Enter the knight's starting position: ").split())
+            input_x, input_y = input_position_to_board_position((input_x, input_y), dim_y)
+            if input_x not in range(0, dim_x) or input_y not in range(0, dim_y):
                 print("Invalid position!")
                 continue
             else:
@@ -69,53 +32,123 @@ def valid_position(matrix: list) -> (int, int):
         except ValueError:
             print("Invalid position!")
 
-    return num1, num2
+    return input_x, input_y
 
 
 # The knight moves in an L-shape, so it has to move 2 squares horizontally and 1 square vertically,
 # or 2 squares vertically and 1 square horizontally.
 # Check all 8 possible moving directions from the starting position.
-def move_directions(x: int, y: int, matrix: list) -> list:
+def generate_possible_move_positions(input_x: int, input_y: int, dim_x: int, dim_y: int, visited_positions: list) -> list:
     moves = [(2, 1), (2, -1), (-2, 1), (-2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2)]
-    landing_position = []
+    positions = []
     for i, j in moves:
-        move_x = x + i
-        move_y = y + j
-        if 0 < move_x <= len(matrix[0]) and 0 < move_y <= len(matrix):
-            landing_position.append((move_x, move_y))
+        move_x = input_x + i
+        move_y = input_y + j
+        if 0 <= move_x < dim_x and 0 <= move_y < dim_y:
+            positions.append((move_x, move_y))
 
-    return landing_position
+    possible_positions = []
+    for p in positions:
+        if p not in visited_positions:
+            possible_positions.append(p)
+
+    return possible_positions
+
+
+def print_chess_board(dim_x: int, dim_y: int, current_position: tuple, visited_positions: list, possible_move_positions: list):
+    num_cells = dim_x * dim_y
+
+    if num_cells >= 100:
+        boarder_length = dim_x * 4 + 3
+        cell_length = 3
+    elif 100 > num_cells >= 10:
+        boarder_length = dim_x * 3 + 3
+        cell_length = 2
+    else:
+        boarder_length = dim_x * 2 + 3
+        cell_length = 1
+
+    print(" " + boarder_length * "-")
+
+    for y in range(dim_y):
+        board_x_strs = [str(dim_y - y) + "|"]
+        for x in range(dim_x):
+            if (x, y) == current_position:
+                board_x_strs.append(((cell_length - 1) * " ") + "X")
+            elif (x, y) in visited_positions:
+                board_x_strs.append(((cell_length - 1) * " ") + "*")
+            elif (x, y) in possible_move_positions:
+                next_possible_move_positions = generate_possible_move_positions(x, y, dim_x, dim_y, visited_positions)
+                board_x_strs.append(((cell_length - 1) * " ") + str(len(next_possible_move_positions) - 1))
+            else:
+                board_x_strs.append(cell_length * "_")
+        board_x_strs.append("|")
+
+        print(*board_x_strs)
+
+    print(" " + boarder_length * "-")
+
+    board_x_strs = [(" " * (cell_length - 1)) + str(i) for i in range(1, dim_x + 1)]
+    board_x_strs.insert(0, "  ")
+    print(*board_x_strs)
+
+
+def valid_move(dim_x: int, dim_y: int, visited_positions: list, possible_move_positions: list) -> (int, int):
+    invalid_input = False
+    while not invalid_input:
+        try:
+            move_x, move_y = map(int, input("Enter your next move: ").split())
+            move_x, move_y = input_position_to_board_position((move_x, move_y), dim_y)
+            if move_x not in range(0, dim_x) or move_y not in range(0, dim_y):
+                print("Invalid move!", end=" ")
+                continue
+            elif (move_x, move_y) in visited_positions:
+                print("Invalid move!", end=" ")
+                continue
+            elif (move_x, move_y) not in possible_move_positions:
+                print("Invalid move!", end=" ")
+                continue
+            else:
+                invalid_input = True
+        except ValueError:
+            print("Invalid move!", end=" ")
+
+    return move_x, move_y
+
+
+def play_knight_moves(input_x: int, input_y: int, dim_x: int, dim_y: int, visited_positions: list):
+
+    while True:
+        possible_move_positions = generate_possible_move_positions(input_x, input_y, dim_x, dim_y, visited_positions)
+
+        left_positions = dim_x * dim_y - len(visited_positions)
+        if left_positions > 0:
+            if not possible_move_positions:
+                print("No more possible moves!")
+                print("Your knight visited {} squares!".format(len(visited_positions)))
+                break
+        elif left_positions == 0:
+            print("What a great tour! Congratulations!")
+            break
+
+        next_x, next_y = valid_move(dim_x, dim_y, visited_positions, possible_move_positions)
+        current_position = (next_x, next_y)
+        next_possible_move_positions = generate_possible_move_positions(next_x, next_y, dim_x, dim_y, visited_positions)
+        print_chess_board(dim_x, dim_y, current_position, visited_positions, next_possible_move_positions)
+
+        input_x, input_y = next_x, next_y
+        visited_positions.append((input_x, input_y))
 
 
 def main():
-    dimension1, dimension2 = valid_dimensions()
-    board = chess_board(dimension1, dimension2)
-    start_position1, start_position2 = valid_position(board)
-
-    if dimension1 * dimension2 >= 100:
-        board[- start_position2][start_position1 - 1] = "  X"
-    elif 100 > dimension1 * dimension2 >= 10:
-        board[- start_position2][start_position1 - 1] = " X"
-    else:
-        board[- start_position2][start_position1 - 1] = "X"
-
-    moves = move_directions(start_position1, start_position2, board)
-    print(moves)
-    for h, v in moves:
-        if dimension1 * dimension2 >= 100:
-            board[-v][h - 1] = f"  {len(move_directions(h, v, board)) - 1}"
-        elif 100 > dimension1 * dimension2 >= 10:
-            board[-v][h - 1] = f" {len(move_directions(h, v, board)) - 1}"
-        else:
-            board[-v][h - 1] = f"{len(move_directions(h, v, board)) - 1}"
-
-    knight_move = boarder_chess_board(board)
-    print("\nHere are the possible moves:")
-    for row in knight_move:
-        if isinstance(row, str):
-            print(row)
-        else:
-            print(*row)
+    dim_x, dim_y = valid_dimensions()
+    input_x, input_y = valid_position(dim_x, dim_y)
+    current_position = (input_x, input_y)
+    visited_positions = []
+    possible_move_positions = generate_possible_move_positions(current_position[0], current_position[1], dim_x, dim_y, visited_positions)
+    print_chess_board(dim_x, dim_y, current_position, visited_positions, possible_move_positions)
+    visited_positions.append(current_position)
+    play_knight_moves(input_x, input_y, dim_x, dim_y, visited_positions)
 
 
 if __name__ == "__main__":
